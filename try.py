8e3f8eac6b19a1e8f08946eba1c703711b127d58
JStalyn <estalin_111@hotmail.com>
2022-06-29 11:11:59 -0500

    actuaolizacion primer commint

---
+++ try.py
@@ -0,0 +1,12 @@
+
+def run ():
+    high = int(input('text your high on cm '))
+    ancho = int(input('text hegiht '))
+    area = high * ancho /2
+    print(area)
+    print('it is wok!')
+
+
+
+if __name__ == '__main__':
+    run()
